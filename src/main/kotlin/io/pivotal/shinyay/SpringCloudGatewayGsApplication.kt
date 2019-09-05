625784ef9da7d7011eb7bced9767cec1f098b610
shinyay <shinya.com@gmail.com>
2019-09-05 10:22:03 +0900

    Add routing rule for gateway

--- src/main/kotlin/io/pivotal/shinyay/SpringCloudGatewayGsApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/SpringCloudGatewayGsApplication.kt
@@ -2,9 +2,20 @@ package io.pivotal.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringCloudGatewayGsApplication
+class SpringCloudGatewayGsApplication {
+	@Bean
+	fun routing(builder: RouteLocatorBuilder) : RouteLocator = builder.routes()
+			.route { p
+				-> p.path("/get")
+					.filters { f -> f.addRequestHeader("Hello", "World") }
+					.uri("http://httpbin.org")
+			}.build()
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudGatewayGsApplication>(*args)
