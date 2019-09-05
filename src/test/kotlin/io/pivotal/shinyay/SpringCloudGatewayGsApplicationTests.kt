f4515079058075943da8c1f2ce77478dd8580e80
shinyay <shinya.com@gmail.com>
2019-09-05 10:22:38 +0900

    Add test for rest end point

--- src/test/kotlin/io/pivotal/shinyay/SpringCloudGatewayGsApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/SpringCloudGatewayGsApplicationTests.kt
@@ -1,16 +1,22 @@
 package io.pivotal.shinyay
 
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
-class SpringCloudGatewayGsApplicationTests {
+@ExtendWith(SpringExtension::class)
+@WebMvcTest()
+class SpringCloudGatewayGsApplicationTests(@Autowired val mockMvc: MockMvc) {
 
 	@Test
-	fun contextLoads() {
+	fun routingTest() {
+		mockMvc.perform(get("/get"))
+				.andExpect(status().isOk)
 	}
 
 }
